8b98708c4815f8ebd5cdf25c55477895bccf75e5
Dedulkaa <55138703+Dedulkaa@users.noreply.github.com>
2019-09-16 10:09:27 +0300

    Add several of functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,26 @@
 package ru.stqa.pft.sandbox;
-
 public class MyFirstProgram {
-
   public static void main(String[] args){
-    System.out.println("Hello, world!!");
+    hello( "world");
+    hello("user");
+    hello("alexei");
+
+    double v = 1.0/2;
+    System.out.println("Square with side  " + v + " = " + area(v));
+
+    double a = 1;
+    double b = 3;
+    System.out.println("Square of rectangle with side  " + a +" and " + b + " = " + area(a,b));
 }
 
+public static void hello(String somebody) {
+  System.out.println("Hello," + somebody + " !");
+}
+
+public static double area (double len) {
+  return len * len;
+}
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
